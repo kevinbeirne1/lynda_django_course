96095caa7a0bcc5d3a0cb1e016cdc8ed54e769ce
kevinbeirne1 <kevingbeirne@gmail.com>
2021-02-23 18:20:43 -0500

    created adoptions app

--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
